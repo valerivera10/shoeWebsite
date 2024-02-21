6b994b0706e7bf899126f346a3cb512b7fb381fb
valerivera10 <156733934+valerivera10@users.noreply.github.com>
2024-02-21 16:41:57 -0500

    feat: :bug: corregi errores en la estructura, html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-import {} from "./module/facturacion.js"
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle('nav-toggle');
+});
